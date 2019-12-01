2a7f36d970cf3833c6d8878400d935e58ee479c9
aupreti1 <anaupreti@gmail.com>
2019-11-30 23:49:20 -0500

    Created the folders and flies and started the connection to DB

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql');
+require('dotenv').config();
+
+var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+connection.connect(function(err) {
+    if(err)throw err;
+    console.log('connected as id' + connection.threadId);
+});
